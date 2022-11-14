037569289148196601662788bf1efa12579aa423
FCHDev <acetige@gmail.com>
2022-11-14 18:04:35 +0100

    first

---
+++ components/Missions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Missions = () => {
+    return (
+        <div id="mission" className="h-screen bg-[#FEDC97] md:pt-28 pt-20 px-10">
+          <h1 className="text-3xl font-[Sono]">NOS MISSIONS</h1>
+        </div>
+    );
+};
+
+export default Missions;
---
+++ components/Nous.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nous = () => {
+    return (
+        <div className="min-h-screen md:mt-24 mt-16 bg-[#31bafd] px-10">
+            <h1 className="text-3xl text-white font-[Sono] pt-4">QUI NOUS SOMMES</h1>
+        </div>
+    );
+};
+
+export default Nous;

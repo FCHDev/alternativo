0954e06b9361595362504f11b23319c0ae816096
FCHDev <acetige@gmail.com>
2023-05-19 16:14:00 +0200

    Mise en page section Mission/Vision/Valeurs revue

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,11 +5,9 @@ const Contact = () => {
 
     return (
         <div id="contact" className="md:mt-0 mt-10 mb-10 md:mb-0 min-h-screen flex flex-col justify-center items-center">
-            <section className="text-gray-600 body-fon lg:pt-28">
-                <h1 className="sm:text-4xl text-3xl font-medium text-center py-10 md:pt-10 lg:pt-0">
-                        <span className="font-bold red-dark">
-                            Contact
-                        </span>
+            <section className="text-gray-600 body-font lg:pt-28">
+                <h1 className="sm:text-4xl text-3xl font-medium text-center md:pt-10 lg:pt-0">
+                        <span className="font-bold red-dark">Contact</span>
                 </h1>
                 {/*<a href="https://calendly.com/alternativo">*/}
                 {/*    Déjà en contact ?*/}
